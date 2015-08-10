0d59ba05def1ed273b6fd607a79aae76c7e0d5ea
João Paulo Pizani Flor <joaopizani@gmail.com>
2015-08-10 22:45:06 +0200

    Put download of vim-plug bootstrap file in install scripts

--- install-basic.sh
+++ install-basic.sh
@@ -7,6 +7,9 @@ cd "${DIR}"
 git submodule update --init --recursive --remote
 cd -
 
+VIMPLUGURL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+curl -fLo "${DIR}/runtime/bundle/plug.vim" --create-dirs "${VIMPLUGURL}"
+
 ln -s -f "${DIR}/_nvimrc"  "${HOME}/_nvimrc"
 
 vim +PlugInstall +qall
--- install-haskell-coq-agda.sh
+++ install-haskell-coq-agda.sh
@@ -8,6 +8,9 @@ git checkout extra-haskell-coq-agda
 git submodule update --init --recursive --remote
 cd -
 
+VIMPLUGURL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+curl -fLo "${DIR}/runtime/bundle/plug.vim" --create-dirs "${VIMPLUGURL}"
+
 ln -s -f "${DIR}/_nvimrc"  "${HOME}/_nvimrc"
 
 vim +PlugInstall +qall
